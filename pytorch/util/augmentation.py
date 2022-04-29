13168156afd6ff74141f41caaf43a0cd16469aa9
seohl16 <selim@student.42seoul.kr>
2022-04-29 04:09:19 +0000

    [fix] aug file

--- pytorch/util/augmentation.py
+++ pytorch/util/augmentation.py
@@ -15,6 +15,6 @@ def get_valid_transform(args):
 
 def get_test_transform(args):
     return A.Compose([
-                    A.Resize(width=256, height=256),
+                    # A.Resize(width=args["RESIZE"][0], height=args["RESIZE"][1]),
                     ToTensorV2()
                     ])
