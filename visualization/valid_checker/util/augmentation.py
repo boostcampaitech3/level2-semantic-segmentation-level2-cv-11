d96b85789383a461b922bbb6d7a96d5742f948eb
sseunghyuns <allindata96@gmail.com>
2022-04-29 04:24:46 +0000

    [feat] valid 추론 결과 확인 코드

---
+++ visualization/valid_checker/util/augmentation.py
@@ -0,0 +1,20 @@
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+def get_train_transform(args):
+    return A.Compose([
+                    # A.Resize(width=args["RESIZE"][0], height=args["RESIZE"][1]),
+                    ToTensorV2()
+                    ])
+
+def get_valid_transform(args):
+    return A.Compose([
+                    # A.Resize(width=args["RESIZE"][0], height=args["RESIZE"][1]),
+                    ToTensorV2()
+                    ])
+
+def get_test_transform(args):
+    return A.Compose([
+                    # A.Resize(width=args["RESIZE"][0], height=args["RESIZE"][1]),
+                    ToTensorV2()
+                    ])
